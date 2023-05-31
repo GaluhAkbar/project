92d6e5f1561ad1b9beb46462c494f8b1eb44713d
GaluhAkbar <66367581+GaluhAkbar@users.noreply.github.com>
2023-05-31 14:39:29 +0700

    Add files via upload

---
+++ indonesia.php
@@ -0,0 +1,38 @@
+<?php
+
+//menu utama
+define ("language", 'Bahasa');
+define ("home", 'Beranda');
+define ("services", 'Produk & Pelayanan');
+define ("gallery", 'Galleri');
+define ("aboutus", 'Tentang Kami');
+define ("contactus", 'Kontak');
+
+//tombol
+define ("moredetail", "Lihat Produk & Pelayanan Kami");
+define ("buttoncontinue", "lihat Galeri Kami");
+define ("buttonsendemail", "Kirim Pesan");
+
+//judul
+define ("labelgallery", "Galeri");
+define ("labelgallerydescription", "Galeri Foto Produk dan Pelayanan Kami");
+define ("labelservices", "Produk & Pelayanan");
+define ("labelaboutus", "Sekilas Tentang Kami");
+define ("labelcontactus", "Kontak Kami");
+
+//form email
+define ("placename", "Nama Anda...");
+define ("placesubject", "Subjek...");
+define ("placeemail", "Alamat Email Anda...");
+define ("placemessage", "Isi Pesan...");
+define ("buttonsendmail", "Kirim Pesan");
+
+//footer
+define ("labellocation", "Lokasi");
+define ("labelinfo", "Informasi Lengkap");
+define ("labelheadoffice", "Kantor Pusat");
+define ("labelphone", "Telepon");
+define ("labelemail", "Email");
+define ("labelsosmed", "Media Sosial");
+
+?>
